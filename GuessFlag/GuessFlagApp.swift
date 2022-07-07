3161ca464fb8bd3e1a70280ebbad86de77c3528c
RozhinaNadya <zhuk.09@mail.ru>
2022-07-07 14:52:41 +0200

    Initial Commit

---
+++ GuessFlag/GuessFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessFlagApp.swift
+//  GuessFlag
+//
+//  Created by Надежда on 2022-07-07.
+//
+
+import SwiftUI
+
+@main
+struct GuessFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
