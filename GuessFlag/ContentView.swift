2331c9e00552e2c3265d0bf7341eee9f6b21f6f9
RozhinaNadya <zhuk.09@mail.ru>
2022-07-07 15:14:51 +0200

    Added countries array and texts

--- GuessFlag/ContentView.swift
+++ GuessFlag/ContentView.swift
@@ -8,9 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var countries = ["UK", "USA", "Bangladesh", "Germany", "Argentina", "Brazil", "Canada", "Greece", "Sweden"] .shuffled() //перемешаем
+    @State private var indexOfRandomCountries = Int.random(in: 0...2) //чтобы на экране было 3 варианта флага
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .yellow]), startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("Choose the flag:")
+                    .foregroundColor(.white)
+                    .font(.headline)
+                Text(countries[indexOfRandomCountries])
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+            }
+        }
     }
 }
 
