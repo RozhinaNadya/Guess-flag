6876b591d9b0d0f0160aee09720f7f7c69c390c7
RozhinaNadya <zhuk.09@mail.ru>
2022-07-07 15:37:25 +0200

    Added flags on the screen

--- GuessFlag/ContentView.swift
+++ GuessFlag/ContentView.swift
@@ -10,20 +10,37 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var countries = ["UK", "USA", "Bangladesh", "Germany", "Argentina", "Brazil", "Canada", "Greece", "Sweden"] .shuffled() //перемешаем
-    @State private var indexOfRandomCountries = Int.random(in: 0...2) //чтобы на экране было 3 варианта флага
+    @State private var indexOfRandomCountries = Int.random(in: 0...2) //тк на экране будет 3 варианта флага
     
     var body: some View {
         ZStack {
             LinearGradient(gradient: Gradient(colors: [.blue, .yellow]), startPoint: .top, endPoint: .bottom)
                 .edgesIgnoringSafeArea(.all)
+            
             VStack {
-                Text("Choose the flag:")
-                    .foregroundColor(.white)
-                    .font(.headline)
-                Text(countries[indexOfRandomCountries])
-                    .foregroundColor(.white)
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
+             //   Spacer()
+                VStack {
+                    Text("Choose the flag:")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                    Text(countries[indexOfRandomCountries])
+                        .foregroundColor(.white)
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                }
+                
+                ForEach(0..<3) { number in
+                    Button(action: {
+                        //action
+                    }) {
+                        Image(countries[number])
+                            .frame(width: 250, height: 130)
+                            .clipShape(Capsule())
+                            .overlay(Capsule()
+                                .stroke(Color.black, lineWidth: 1))
+                    }
+                   // Spacer()
+                }
             }
         }
     }
